99857460769a3faac331a05fc314f048e55a348e
code-lova <ebinuga@hotmail.com>
2025-03-17 11:54:55 +0100

    doc: fix marquee

--- components/landing/Landing.jsx
+++ components/landing/Landing.jsx
@@ -1,18 +1,15 @@
 "use client";
-import Nav from "./Nav";
 import Hero from "./Hero";
 import Features from "./Features";
 import HowitWorks from "./HowitWorks";
 import AboutUs from "./AboutUs";
 import Services from "./Services";
 import Team from "./Team";
-import Pricing from "./Pricing";
 import Faq from "./Faq";
 import ContactUs from "./ContactUs";
 import Subscription from "./Subscription";
-import Connect from "./Connect";
 import Footer from "./Footer";
-import Marquee from "@components/core/marquee";
+import Marquee from "@components/core/Marquee";
 
 const Landing = () => {
   return (
