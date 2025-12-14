d0861e9196250d85f0190bafe64344ef53dbdf1e
code-lova <ebinuga@hotmail.com>
2025-12-14 01:23:38 +0100

    doc: privacy policy and terms fix

--- app/(accounts)/admin/support-ticket/reply/page.jsx
+++ app/(accounts)/admin/support-ticket/reply/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { Suspense } from "react";
 import TicketReply from "@components/accounts/admin/TicketReply";
 import TicketReplySkeleton from "@components/core/skeleton/TicketReplySkeleton";
